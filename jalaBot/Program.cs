e5f8baf9087fe5407e8da8922db738a6a9d65ba9
datboi26 <pineapplesgg@gmail.com>
2017-07-05 16:46:38 -0400

    jalaBot

---
+++ jalaBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace jalaBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().GetAwaiter().GetResult();
+        }
+
+        public static async Task Run()
+        {
+            var discord = new DiscordClient(new DiscordConfig
+            {
+                AutoReconnect = true,
+                DiscordBranch = Branch.Stable,
+                LargeThreshold = 250,
+                LogLevel = LogLevel.Critical,
+                Token = "MzMyMjUxODE0ODkwMTc2NTMz.DD7ZMQ.WXmzXFRYYMegSrEtTNKxmpM92gI",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = false
+            });
+
+            await discord.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
