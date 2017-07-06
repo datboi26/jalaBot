0b843ddabc056306cf2ceec7b2b9f49bbe80f4c4
datboi26 <pineapplesgg@gmail.com>
2017-07-05 20:38:55 -0400

    jalaBot

--- jalaBot/Program.cs
+++ jalaBot/Program.cs
@@ -18,7 +18,7 @@ namespace jalaBot
 
             client.MessageReceived += MessageReceived;
 
-            string token = "MzMyMjUxODE0ODkwMTc2NTMz.DD7ZMQ.WXmzXFRYYMegSrEtTNKxmpM92gI"; 
+            string token = ""; 
             await client.LoginAsync(Discord.TokenType.Bot, token);
             await client.StartAsync();            
             await Task.Delay(-1);
